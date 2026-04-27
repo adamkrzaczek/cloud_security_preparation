cb75cd917b6167926d660cd32410cda220e228be
adamkrzaczek <adam.krzaczek@gmail.com>
2026-04-27 21:21:32 +0200

    First solve, first check

---
+++ CloudGoat/checks/imds_v1.py
@@ -0,0 +1,24 @@
+from botocore.exceptions import ClientError
+
+def run(session, region: str, fix_flag: bool):
+        ec2 = session.client("ec2", region_name=region)
+        paginator = ec2.get_paginator("describe_instances")
+        for page in paginator.paginate():
+            for reservation in page.get("Reservations",[]):
+                for instance in reservation.get("Instances",[]):
+                    instance_id = instance.get('InstanceId')
+                    metadata_option = instance.get("MetadataOptions",{})
+                    http_token = metadata_option.get("HttpTokens")
+                    if http_token == "optional":
+                        print(f"[!] {region}:IMDSv1 ENABLED on {instance_id} ({instance.get('InstanceType')})")
+                        if fix_flag :
+                            fix(ec2,instance_id)
+
+def fix(ec2,instanceId: str):
+    try:
+        ec2.modify_instance_metadata_options(InstanceId=instanceId,HttpTokens="required")
+        print(f"[+] FIXED {instanceId}. Now it use IMDSv2")
+    except ClientError as e:
+        error = e.response.get("Error",{})
+        error_msg = error.get("Message","Unknown AWS    Error")
+        print(f"[X] Couldn't fix the issue: {instanceId}: {error_msg}")
---
+++ CloudGoat/main.py
@@ -0,0 +1,23 @@
+import argparse
+from utils import get_regions,get_session
+
+#IMPORT CHECKS
+from checks import imds_v1
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--fix",action="store_true",help="Automatic Fix")
+    parser.add_argument("--region",nargs="+",default=["us-east-1"], help="List regions to check")
+
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+    session = get_session()
+    regions = get_regions(args.region)
+    for region in regions:
+        
+        #Check 1: IMDSv1
+        imds_v1.run(session,region,args.fix)
+
+if __name__ == "__main__":
+    main()
